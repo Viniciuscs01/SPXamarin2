6e101bf0ed29c9fa5a03af110d36a70f1e30ae03
Vinícius Carvalho <v.cs@outlook.com.br>
2016-09-22 11:32:43 -0300

    Continuação da Main

---
+++ SPXamarin2/Helper/AuthenticationHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Linq;
+
+namespace OfficeSolutions.SharePoint
+{
+    public class AuthenticationHelper
+    {
+        public const string Authority = "https://login.windows.net/common";
+        public static Uri returnUri = new Uri("http://xam-demo-redirect");
+        public static string clientId = "9e9ab416-f20b-4e26-9b24-d6bd3c1882ce";
+        public static AuthenticationContext authContext = null;
+        public static string SharePointURL = "https://classsolutions.sharepoint.com/";
+
+        public static async Task<AuthenticationResult> GetAccessToken(string serviceResourceId, PlatformParameters param)
+        {
+            authContext = new AuthenticationContext(Authority);
+            if (authContext.TokenCache.ReadItems().Any())
+                authContext = new AuthenticationContext(authContext.TokenCache.ReadItems().First().Authority);
+            var authResult = await authContext.AcquireTokenAsync(serviceResourceId, clientId, returnUri, param);
+            return authResult;
+        }
+    }
+}
+
--- SPXamarin2/MainActivity.cs
+++ SPXamarin2/MainActivity.cs
@@ -5,6 +5,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using OfficeSolutions.SharePoint;
 
 namespace SPXamarin2
 {
@@ -12,6 +17,12 @@ namespace SPXamarin2
     public class MainActivity : Activity
     {
         int count = 1;
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            AuthenticationAgentContinuationHelper.SetAuthenticationAgentContinuationEventArgs(requestCode, resultCode, data);
+        }
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -27,9 +38,105 @@ namespace SPXamarin2
             button.Click += Button_Click;
         }
 
-        private void Button_Click(object sender, EventArgs e)
+        private async void Button_Click(object sender, EventArgs e)
         {
-            
+            var authResult = await AuthenticationHelper.GetAccessToken(AuthenticationHelper.SharePointURL,
+                new PlatformParameters(this));
+            //await CreateList(authResult.AccessToken);
+            //await CreateItems(authResult.AccessToken);
+            await FetchListItems(authResult.AccessToken);
+        }
+
+        //protected async Task<bool> CreateList(string token)
+        //{
+        //    var client = new HttpClient();
+        //    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        //    var mediaType = new MediaTypeWithQualityHeaderValue("application/A");
+        //    mediaType.Parameters.Add(new NameValueHeaderValue("odata", "verbose"));
+        //    client.DefaultRequestHeaders.Accept.Add(mediaType);
+        //    var body = "{\"__metadata\":{\"type\":\"SP.List\"},\"AllowContentTypes\":true,\"BaseTemplate\":107,\"ContentTypesEnabled\":true,\"Description\":\"Tasks by Xamarin.Android\",\"Title\":\"TasksByAndroid\"}";
+        //    var contents = new StringContent(body);
+        //    contents.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json;odata=verbose");
+        //    try
+        //    {
+        //        var postResult = await client.PostAsync("https://classsolutions.sharepoint.com/sites/Vinicius/_api/web/lists/", contents);
+        //        var result = postResult.EnsureSuccessStatusCode();
+        //        Toast.MakeText(this, "List created successfully! Seeding tasks.", ToastLength.Long).Show();
+        //        return true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Toast.MakeText(this, "List already exists! Fetching tasks.", ToastLength.Long).Show();
+        //        return false;
+
+        //    }
+        //}
+
+        //protected async Task<bool> CreateItems(string token)
+        //{
+        //    var client = new HttpClient();
+        //    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        //    var mediaType = new MediaTypeWithQualityHeaderValue("application/json");
+        //    mediaType.Parameters.Add(new NameValueHeaderValue("odata", "verbose"));
+        //    client.DefaultRequestHeaders.Accept.Add(mediaType);
+
+        //    var itemToCreateTitle = "Item created on: " + DateTime.Now.ToString("dd/MM HH:mm");
+        //    var body = "{\"__metadata\":{\"type\":\"SP.Data.TasksByAndroidListItem\"},\"Title\":\"" + itemToCreateTitle + "\",\"Status\": \"Not Started\"}";
+        //    var contents = new StringContent(body);
+        //    contents.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json;odata=verbose");
+        //    try
+        //    {
+
+        //        var postResult = await client.PostAsync("https://classsolutions.sharepoint.com/sites/Vinicius/_api/web/lists/GetByTitle('TasksByAndroid')/items", contents);
+        //        var result = postResult.EnsureSuccessStatusCode();
+        //        if (result.IsSuccessStatusCode)
+        //            Toast.MakeText(this, "List item created successfully!", ToastLength.Long).Show();
+        //        return true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        var msg = "Unable to create list item. " + ex.Message;
+        //        Toast.MakeText(this, msg, ToastLength.Long).Show();
+        //        return false;
+        //    }
+        //}
+
+        protected async Task<bool> FetchListItems(string token)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var mediaType = new MediaTypeWithQualityHeaderValue("application/json");
+            mediaType.Parameters.Add(new NameValueHeaderValue("odata", "verbose"));
+            client.DefaultRequestHeaders.Accept.Add(mediaType);
+            try
+            {
+                var result = await client.GetStringAsync("https://classsolutions.sharepoint.com/sites/Vinicius/_api/web/lists/GetByTitle('TasksByAndroid')/items");
+                var data = JsonConvert.DeserializeObject<OfficeSolutions.SharePoint.Model.ListItemModels>(result);
+                ListAdapter = new ListItemAdapter(this, data.D.Results);
+            }
+
+            catch (Exception ex)
+            {
+                var msg = "Unable to fetch list items. " + ex.Message;
+                Toast.MakeText(this, msg, ToastLength.Long).Show();
+            }
+            return true;
+        }
+
+        public class Data
+        {
+            public Results d { get; set; }
+        }
+
+        public class Results
+        {
+            public SharePointListItem[] results { get; set; }
+        }
+
+        public class SharePointListItem
+        {
+            public string id { get; set; }
+            public string Title { get; set; }
         }
     }
 }
